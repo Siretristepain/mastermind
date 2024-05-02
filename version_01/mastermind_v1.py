d5e7e248a03ebfe0778e72a88a3dae14bb9b0a41
Raphael <raphael.ollier7@gmail.com>
2024-05-02 19:30:59 +0200

    Fin de la fonction comparaison + nouvelle idee pour print_board

--- version_01/mastermind_v1.py
+++ version_01/mastermind_v1.py
@@ -65,7 +65,7 @@ def comparaison(nb1 : list , nb2 : int):
     # Now I convert all items in this list in integer
     nb2 = [(lambda x: int(x))(x) for x in nb2]
 
-    # I create a copy of the code to find because  if I delete some number inside, the whole suite will move to the left.
+    # I create a copy of the code to find because if I delete some number inside, the whole suite will move to the left.
     copy_nb1 = nb1.copy()
 
     # First I will check if the numbers are good, without taking care of the position
@@ -76,16 +76,21 @@ def comparaison(nb1 : list , nb2 : int):
             copy_nb1.remove(number) # we have to remove the good number from the nb1_copy to not count twice a good number.
 
     # Now I want to count the number of good number AND at the good position
+    ind_good_position = [] # I use this list to stock the index of the good numbers at the correct position
     good_position = 0
     for i in range(len(nb2)):
         if nb2[i] == nb1[i]:
             good_position +=1
-            good_number -= 1 # Very important to retire one here to not count twice a number both in good and good+good_position (because good_number is already imlicit in good_position)
+            good_number -= 1 # Very important to retire one here to not count twice a number both in good and good+good_position (because good_number is already implicit in good_position)
+            ind_good_position.append(i) # I add the index of the good number at the good position in my list
     
     print(f"Good number : {good_number}")
     print(f"Good position : {good_position}")
 
-def print_board():
+    return nb2
+
+
+def print_board(attempt: list):
     """
     This function is used to print the board game.
     I want something like this :
@@ -95,16 +100,24 @@ def print_board():
     | ? | ? | ? | ? |
     |___|___|___|___|
 
-    Where "?" are the numbers not find by the player yet.
-    When the player find a number, the "?" will be replace by the value of this number.
+    Where "?" are the numbers given by the player
+
+    Args:
+        attempt (list) : the code given by the player
 
     Returns:
         board [ ] : the board game.
     """
+
+    print(f"_________________")
+    print(f"|   |   |   |   |")
+    print(f"| {attempt[0]} | {attempt[1]} | {attempt[2]} | {attempt[3]} |")
+    print(f"|___|___|___|___|")
 
     pass
 
 
 if __name__ == '__main__':
     # start_game()
-    comparaison([1,2,3,4], '4213')
+    # comparaison([1,2,3,4], '4213')
+    print_board([1,2,3,4])
